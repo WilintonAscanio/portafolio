13a33e09c1e5a5f0b2d1b0c2fcf9688ec5e4b6fe
WilintonAscanio <wilintonascanio12@hotmail.com>
2023-09-29 14:48:24 -0500

    add resume button

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <form>
+        
+    </form>
+  )
+}
+
+export default Contact
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,16 +3,19 @@ import './home.scss'
 import Navbar from '../navbar/Navbar'
 import photo from '../../assets/profile.jpeg'
 import SocialNetworks from './SocialNetworks'
+import pdf from '../../assets/wilinton.pdf'
 
 const Home = () => {
+
     return (
         <article className='home'>
             <SocialNetworks />
             <Navbar />
             <div className='home__info'>
                 <section>
-                    <strong>Wilinton Ascanio Toro</strong>
+                    <strong>Hola, soy Wilinton Ascanio Toro</strong>
                     <h2>Web Developer</h2>
+                    <a href={pdf} target="_blank" download='CVWilintonAscanioToro.pdf' className='btnDownload'>DOWNLOAD RESUME</a>
                     <p>
                         I'm a web development professional with experience in building online applications. My overall skill set includes the use of technologies like HTML5, CSS3, JavaScript, and ReactJS to create engaging and useful user experiences. Additionally, I have knowledge of backend technologies including Java, Python, Docker, APIs, and related as well as unrelated data bases.
                         Along with my technical skills, I also have strong teamwork abilities and excellent communication skills. I'm versatile and am knowledgeable about using agile project management methodologies.
