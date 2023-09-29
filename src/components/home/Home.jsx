877926f2fca66875ce6fe686001d31992564f171
WilintonAscanio <wilintonascanio12@hotmail.com>
2023-09-29 15:34:40 -0500

    Add about me component, styles

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from '../navbar/Navbar'
 import photo from '../../assets/profile.jpeg'
 import SocialNetworks from './SocialNetworks'
 import pdf from '../../assets/wilinton.pdf'
+import About from '../about/About'
 
 const Home = () => {
 
@@ -13,18 +14,16 @@ const Home = () => {
             <Navbar />
             <div className='home__info'>
                 <section>
-                    <strong>Hola, soy Wilinton Ascanio Toro</strong>
+                    <strong>Hi, I'm Wilinton Ascanio Toro</strong>
                     <h2>Web Developer</h2>
                     <a href={pdf} target="_blank" download='CVWilintonAscanioToro.pdf' className='btnDownload'>DOWNLOAD RESUME</a>
-                    <p>
-                        I'm a web development professional with experience in building online applications. My overall skill set includes the use of technologies like HTML5, CSS3, JavaScript, and ReactJS to create engaging and useful user experiences. Additionally, I have knowledge of backend technologies including Java, Python, Docker, APIs, and related as well as unrelated data bases.
-                        Along with my technical skills, I also have strong teamwork abilities and excellent communication skills. I'm versatile and am knowledgeable about using agile project management methodologies.
-                    </p>
                 </section>
                 <figure>
                     <img src={photo} alt="perfil" />
                 </figure>
             </div>
+            <About />
+
         </article>
     )
 }
